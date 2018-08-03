2a840aaa6d1e417346eb6642a9cdd30a425d427b
Ben Caimano <ben.caimano@10gen.com>
2018-08-03 13:59:49 -0400

    Made the evergreen wrapper nicer still

--- mongodb/evergreen.sh
+++ mongodb/evergreen.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+# Evergreen expects your secret to be in the same file as all other configs.
+# This is bad for sharing your config.
+# So I keep the secret in the normal file and concatenate it with my true config
 REAL_CONF="${HOME}/.evg.comp.yaml"
 1>&2 echo "Using '${REAL_CONF}' as evergreen config..."
 rm "${REAL_CONF}"
@@ -9,7 +12,12 @@ rm "${REAL_CONF}"
 CONFS=()
 IFS=":" read -ra CONFS <<< "${EVG_CONFS}"
 for CONF in "${CONFS[@]}"; do
-  cat "${CONF}" >> "${REAL_CONF}" || true
+  cat "${CONF}" >> "${REAL_CONF}" 2>/dev/null || true
 done
 
-evergreen -c "${REAL_CONF}" "${@}"
+EVG_CMD=("evergreen" "-c" "${REAL_CONF}")
+
+# An up-to-date evg cmd is a good thing
+1>&2 echo "Checking for updates..."
+"${EVG_CMD[@]}" get-update --install
+"${EVG_CMD[@]}" "${@}"
